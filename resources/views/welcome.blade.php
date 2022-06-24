8184f8e53bd974e6337dd8be8ea1982f665ed0e9
saavedradev <saavedra.dev@gmail.com>
2022-06-23 21:12:54 -0500

    changes for url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
          <meta charset="utf-8">
          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-         <title>Bootstrap 4 Template with Sidebar Menu</title>
+         <title>LecareSpa</title>
 
 
         <title>Laravel</title>
